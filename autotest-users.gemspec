467c491736c8a8e50a3a61203bf01ae58def1905
Andrey Botalov <botalov.andrey@gmail.com>
2014-07-10 00:25:52 +0300

    Add hooks for post-create/change of user

--- autotest-users.gemspec
+++ autotest-users.gemspec
@@ -2,19 +2,13 @@
 require File.expand_path('../lib/autotest-users/version', __FILE__)
 
 Gem::Specification.new do |gem|
-  gem.authors       = ["Alex Klimenkov"]
-  gem.email         = ["alex.klimenkov89@gmail.com"]
-  gem.description   = %q{Gem for provide user for autotest}
-  gem.summary       = %q{Gem create, update, save some params for users. All users are saved in the Hash and will available, when automated test is running. Users are not saved into database. Today it is not needed.}
-  gem.homepage      = "https://github.com/AlexKlim/autotest-users"
+  gem.authors       = ["Alex Klimenkov", 'Andrey Botalov']
+  gem.email         = ["botalov.andrey@gmail.com"]
+  gem.description   = %q{Gem to provide user for autotest}
+  gem.homepage      = "https://github.com/abotalov/autotest-users"
 
   gem.files         = `git ls-files`.split($\)
-  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.name          = "autotest-users"
   gem.require_paths = ["lib"]
   gem.version       = Autotest::Users::VERSION
-
-  gem.add_dependency("randexp")
-
 end
--- lib/autotest-users.rb
+++ lib/autotest-users.rb
@@ -3,32 +3,22 @@ require "autotest-users/version"
 module Autotest
 
   class << self
-    attr_accessor :email, :password
+    attr_accessor :email
 
     def configure
       yield self
     end
+
+    def on_user_create(user); end
+
+    def on_user_change(user); end
   end
 
   module Users
-    def create_user(name)
-      require "randexp"
-
+    def create_user(name, options = {})
       $users ||= {}
       $users[name] = ActiveSupport::HashWithIndifferentAccess.new
-
-      first_name = /[:first_name:]/.gen
-      last_name = /[:last_name:]/.gen
-      first_name.gsub!("'",'')
-      last_name.gsub!("'",'')
-
-      $users[name].tap do |user|
-        user[:first_name] = first_name
-        user[:last_name] = last_name
-        user[:full_name] = "#{first_name} #{last_name}"
-        generate_email_for(user)
-        user[:password] = Autotest.password
-      end
+      set_user_data(name, options = {})
     end
 
     def get_user(name)
@@ -42,9 +32,6 @@ module Autotest
       user = get_user(name)
       options.with_indifferent_access.each do |key, value|
         user[key] = value
-        if %w(first_name last_name).include?(key)
-          user[:full_name] = "#{user[:first_name]} #{user[:last_name]}"
-        end
       end
       options.values.first
     end
@@ -52,9 +39,9 @@ module Autotest
     def get_user_data(name, *keys)
       user = get_user(name)
       if keys.size == 1
-        user.fetch(key)
+        user.fetch(keys.first)
       else
-        keys.map { user.fetch(key) }
+        keys.map { |key| user.fetch(key) }
       end
     end
 
@@ -71,7 +58,7 @@ module Autotest
     def user_created?(name)
       ($users && $users[name]).nil? ? false : true
     end
-    
+
     def all_users
       $users
     end
@@ -83,7 +70,15 @@ module Autotest
   end
 end
 
-Autotest.configure do |config|
-  config.email = 'email@example.com'
-  config.password = 'password'
+Autotest.email = 'test@example.com'
+
+Autotest.on_user_create do |user|
+  user[:first_name] = 'First'
+  user[:last_name] = 'Last'
+  user[:email] = generate_email_for(user)
+  user[:password] = 'password'
+end
+
+Autotest.on_user_change do |user|
+  user[:full_name] = "#{user[:first_name]} #{user[:last_name]}"
 end
